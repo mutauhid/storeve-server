f5b7290bb19fb4f63822e2fa19cbd14ce45ad9b5
mutauhid30 <mutauhid30@gmail.com>
2023-04-29 16:08:18 +0700

    setting deploy

--- app/users/controller.js
+++ app/users/controller.js
@@ -1,22 +1,21 @@
-const User = require("./model");
-const bcrypt = require("bcryptjs");
+const User = require('./model');
+const bcrypt = require('bcryptjs');
 
 const viewSignin = async (req, res) => {
   try {
-    const alertMessage = req.flash("alertMessage");
-    const alertStatus = req.flash("alertStatus");
+    const alertMessage = req.flash('alertMessage');
+    const alertStatus = req.flash('alertStatus');
 
     const alert = { message: alertMessage, status: alertStatus };
     if (req.session.user === null || req.session.user === undefined) {
-      res.render("admin/users/view_sigin", { alert,
-        title: "Login Page", });
+      res.render('admin/users/view_sigin', { alert, title: 'Login Page' });
     } else {
-      res.redirect("/dashboard");
+      res.redirect('/dashboard');
     }
   } catch (error) {
-    req.flash("alertMessage", `${error.message}`);
-    req.flash("alertStatus", "danger");
-    res.redirect("/");
+    req.flash('alertMessage', `${error.message}`);
+    req.flash('alertStatus', 'danger');
+    res.redirect('/');
   }
 };
 
@@ -26,7 +25,7 @@ const actionSigin = async (req, res) => {
     const user = await User.findOne({ email: email });
 
     if (user) {
-      if (user.status === "Y") {
+      if (user.status === 'Y') {
         const checkPassword = await bcrypt.compare(password, user.password);
         if (checkPassword) {
           req.session.user = {
@@ -35,27 +34,26 @@ const actionSigin = async (req, res) => {
             status: user.status,
             name: user.name,
           };
-          console.log(req.session);
-          res.redirect("/dashboard");
+          res.redirect('/dashboard');
         } else {
-          req.flash("alertMessage", `Wrong Password`);
-          req.flash("alertStatus", "danger");
-          res.redirect("/");
+          req.flash('alertMessage', `Wrong Password`);
+          req.flash('alertStatus', 'danger');
+          res.redirect('/');
         }
       } else {
-        req.flash("alertMessage", `Your account status is not active`);
-        req.flash("alertStatus", "danger");
-        res.redirect("/");
+        req.flash('alertMessage', `Your account status is not active`);
+        req.flash('alertStatus', 'danger');
+        res.redirect('/');
       }
     } else {
-      req.flash("alertMessage", `User not found`);
-      req.flash("alertStatus", "danger");
-      res.redirect("/");
+      req.flash('alertMessage', `User not found`);
+      req.flash('alertStatus', 'danger');
+      res.redirect('/');
     }
   } catch (error) {
-    req.flash("alertMessage", `${error.message}`);
-    req.flash("alertStatus", "danger");
-    res.redirect("/");
+    req.flash('alertMessage', `${error.message}`);
+    req.flash('alertStatus', 'danger');
+    res.redirect('/');
   }
 };
 
@@ -63,16 +61,16 @@ const actionLogout = (req, res) => {
   try {
     if (req.session) {
       req.session.destroy();
-      res.redirect("/");
+      res.redirect('/');
     } else {
-      req.flash("alertMessage", `Please Login First`);
-      req.flash("alertStatus", "danger");
-      res.redirect("/");
+      req.flash('alertMessage', `Please Login First`);
+      req.flash('alertStatus', 'danger');
+      res.redirect('/');
     }
   } catch (error) {
-    req.flash("alertMessage", `${error.message}`);
-    req.flash("alertStatus", "danger");
-    res.redirect("/");
+    req.flash('alertMessage', `${error.message}`);
+    req.flash('alertStatus', 'danger');
+    res.redirect('/');
   }
 };
 
